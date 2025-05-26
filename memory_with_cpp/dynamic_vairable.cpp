2a680a0ee7aa0b0506135a60c7e05b8ee591806f
Hosain <cryptichasan925@gmail.com>
2025-05-26 11:44:01 +0600

    dynamic variable done

---
+++ memory_with_cpp/dynamic_vairable.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h> //all headers
+using namespace std;
+
+void fun () {
+  int p = 10;
+  return;
+}
+int main() {
+  int x = 10;
+  int *p = new int;
+  *p = 100;
+  cout << p << endl;
+  cout << *p << endl;
+  fun();
+  return 0;
+}
