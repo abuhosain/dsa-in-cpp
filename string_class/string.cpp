77d97012a12f64f744df3d3f253d455676f98fe2
Hosain <cryptichasan925@gmail.com>
2025-06-01 13:54:32 +0600

    string

---
+++ string_class/string.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  //  char s[10] = "hello";
+  //  strcpy(s, "world");
+  int x;
+  string s = "hello";
+  string s1 = "world";
+  if(s == s1) {
+      cout << "equal" << endl;
+  } else {
+      cout << "not equal" << endl;
+  }
+   
+   return 0;
+};
