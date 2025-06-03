5a2d78a55d6ede478d8c3246a8dc6f401a75d401
Hosain <cryptichasan925@gmail.com>
2025-06-03 15:23:27 +0600

    append modifirer

---
+++ string_modifier.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   string s = "hello";
+    string s1 = " world";
+    // s+= s1;
+    s.append(s1);
+    cout << s << endl;  
+   return 0;
+};
