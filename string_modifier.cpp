cf357e3b873c7e98e681d144c119124fca8f929e
Hosain <cryptichasan925@gmail.com>
2025-06-03 15:31:14 +0600

    assign done

--- string_modifier.cpp
+++ string_modifier.cpp
@@ -6,8 +6,11 @@ int main(){
     string s1 = " world";
     // s+= s1;
     // s.append(s1);
-    s.push_back('!');
-    s.pop_back();
-    cout << s << endl;  
+    // s.push_back('!');
+    // s.pop_back();
+    // string s2 = "hi";
+    // s1 = s2;
+    s1.assign(s);
+    cout << s1 << endl;  
    return 0;
 };
