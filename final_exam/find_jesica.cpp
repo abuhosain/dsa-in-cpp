a97f51d5d0f66a7dc0ffa23e537f114651319eef
Hosain <cryptichasan925@gmail.com>
2025-06-17 09:58:40 +0600

    find jessica

---
+++ final_exam/find_jesica.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    string S;
+    getline(cin, S); 
+
+    string word;
+    stringstream ss(S); 
+    bool found = false;
+
+    while (ss >> word) {
+        if (word == "Jessica") {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) cout << "YES" << endl;
+    else cout << "NO" << endl;
+
+    return 0;
+}
